98f3608eec1679b62aabf341703d1b4d66f0206f
Patrick Dixon <dixonp1@sou.edu>
2018-05-21 22:16:22 -0700

    initial model

--- model.py
+++ model.py
@@ -2,7 +2,11 @@ import tensorflow as tf
 
 
 def initialzie_weights(shape):
-    w = tf.truncated_normal(shape, stddev=0.01)
+    if len(shape) == 4:
+        std = 2 / (shape[0]*shape[1]*shape[2])
+    else:
+        std = 2 / (shape[0] * shape[1])
+    w = tf.truncated_normal(shape, stddev=std)
     b = tf.zeros([shape[-1]])
     return tf.Variable(w), tf.Variable(b)
 
@@ -15,7 +19,7 @@ def pool(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-def create_classifier(input, settings, training=False):
+def create_classifier(input, settings, keep_prob=1.0, training=False):
     ""
 
     '''   Convolution Layer 1   '''
@@ -23,39 +27,54 @@ def create_classifier(input, settings, training=False):
     W1, b1 = initialzie_weights([w, h, c, nf])
     with tf.name_scope('conv1'):
         conv1 = conv(input, W1)
-        relu1 = tf.nn.relu(conv1 + b1)
-        pool1 = pool(relu1)
+        relu1 = tf.nn.leaky_relu(conv1)
+        bn1 = tf.layers.batch_normalization(relu1, axis=3, training=training)
+        pool1 = pool(bn1)
 
     '''   Convolution Layer 2   '''
     w, h, c, nf = settings['conv2']
     W2, b2 = initialzie_weights([w, h, c, nf])
     with tf.name_scope('conv2'):
         conv2 = conv(pool1, W2)
-        relu2 = tf.nn.relu(conv2 + b2)
-        pool2 = pool(relu2)
+        relu2 = tf.nn.leaky_relu(conv2)
+        bn2 = tf.layers.batch_normalization(relu2, axis=3, training=training)
+        pool2 = pool(bn2)
+
+    '''   Convolution Layer 3   '''
+    w, h, c, nf = settings['conv3']
+    W3, b3 = initialzie_weights([w, h, c, nf])
+    with tf.name_scope('conv3'):
+        conv3 = conv(pool2, W3)
+        relu3 = tf.nn.leaky_relu(conv3)
+        bn3 = tf.layers.batch_normalization(relu3, axis=3, training=training)
 
     '''   Flatten   '''
-    n_out1 = settings['full1']
-    flatten = tf.reshape(pool2, [-1, w*h*nf])
+    w, h, nf = settings['flat']
+    flatten = tf.reshape(bn3, [-1, w*h*nf])
 
     '''   Fully Connected Layer 1   '''
+    n_out1 = settings['full1'][0]
     W3, b3 = initialzie_weights([w*h*nf, n_out1])
     with tf.name_scope('full1'):
         full1 = tf.matmul(flatten, W3)
-        relu3 = tf.nn.relu(full1 + b3)
+        relu3 = tf.nn.leaky_relu(full1 + b3)
+
+    '''   Dropout   '''
+    if training != False:
+        dropout = tf.nn.dropout(relu3, keep_prob=keep_prob)
+    else:
+        dropout = relu3
 
     '''   Fully Connected Layer 2   '''
-    n_out2 = settings['full2']
-    W4, b4 = initialzie_weights([n_out1, n_out2])
+    n_out2 = settings['full2'][0]
+    W4, b4 = initialzie_weights([w*h*nf, n_out2])
     with tf.name_scope('full2'):
-        full2 = tf.matmul(relu3, W4)
+        full2 = tf.matmul(flatten, W4)
         z = full2 + b4
 
     '''   Softmax   '''
-    if training:
-        prediction = z
-    else:
-        prediction = tf.nn.softmax(z)
+    #prediction = tf.nn.softmax(z)
+    prediction = z
 
     return prediction
 
--- train.py
+++ train.py
@@ -3,94 +3,219 @@ import numpy as np
 import os
 import random
 import model
-import cv2
 
-from glob import glob
+from scipy import io
+np.set_printoptions(linewidth=200, threshold=np.nan)
 
-def prep_data(data_dir, test_perc=10, val_perc=10):
-    dataset = {'training': [], 'test': [], 'val': []}
-    labels = {}
 
-    folders = glob(data_dir)
-    classes = [os.path.split(f)[1] for f in folders]
-    classes.sort()
+CLASSES = 26
 
-    for folder in folders:
-        _, c = os.path.split(folder)
-        files = glob(folder)
-        for f in files:
-            l = np.zeros(len(classes))
-            l[classes.index(c)] = 1
-            labels[f] = l
+def extract_data(data_file):
+    data = io.loadmat(data_file)['dataset']
 
-        random.shuffle(files)
-        test_part = int(len(files) * test_perc)
-        val_part = int(len(files) * val_perc) + test_part
+    train_data = data[0][0][0][0][0][0]
+    train_data = train_data.astype(np.float32)
+    train_labels = data[0][0][0][0][0][1]
 
-        dataset['test'] += (files[:test_part])
-        dataset['val'] += (files[test_part:val_part])
-        dataset['training'] += (files[val_part:])
+    test_data = data[0][0][1][0][0][0]
+    test_data = test_data.astype(np.float32)
+    test_labels = data[0][0][1][0][0][1]
 
-    return dataset, labels, len(classes)
 
-def load_data(data, labels):
-    batch = []
-    gtruth = []
-    for f in data:
-        batch.append(cv2.imread(f))
-        gtruth.append(labels[f])
-    return batch, gtruth
+    val_set_size = test_data.shape[0]
+    val_index = train_data.shape[0] - val_set_size
+    val_data = train_data[val_index:]
+    val_labels = train_labels[val_index:]
 
-data_dir = 'data'
+    train_data = train_data[:val_index]
+    train_labels = train_labels[:val_index]
 
-dataset, labels, num_classes = prep_data(data_dir)
+    #train_data, train_labels = parse_nums(train_data, train_labels)
+    #val_data, val_labels = parse_nums(val_data, val_labels)
+    #test_data, test_labels = parse_nums(test_data, test_labels)
 
-settings = {'conv1': [],
-            'conv2': [],
-            'full1': [],
-            'full2': [1024, num_classes]}
+    train_labels = convert_to_one_hot(train_labels, CLASSES)
+    val_labels = convert_to_one_hot(val_labels, CLASSES)
+    test_labels = convert_to_one_hot(test_labels, CLASSES)
+
+    train_data = train_data.reshape(-1, 28, 28, 1, order='F') / 255
+    test_data = test_data.reshape(-1, 28, 28, 1, order='F') / 255
+    val_data = val_data.reshape(-1, 28, 28, 1, order='F') / 255
+
+    dataset = {'train': (train_data, train_labels),
+               'val': (val_data, val_labels),
+               'test': (test_data, test_labels)}
+
+    return dataset
+
+
+def convert_to_one_hot(labels, num_classes):
+    labels -= 1
+    num_labels = labels.shape[0]
+    offset = np.arange(num_labels) * num_classes
+    new_labels = np.zeros((num_labels, num_classes))
+    new_labels.flat[offset + labels.ravel()] = 1
+    return new_labels
+
+
+def parse_nums(img, labels):
+    idx = (labels > 9).reshape(-1)
+    new_img = img[idx]
+    new_labels = labels[idx] - 10
+    return new_img, new_labels
+
+
+def shuffle_data(imgs, labels):
+    shuff = np.arange(imgs.shape[0])
+    np.random.shuffle(shuff)
+    shuff_imgs = imgs[shuff]
+    shuff_labels = labels[shuff]
+    return shuff_imgs, shuff_labels
+
+
+def shift_img(img):
+    rx = random.randint(-4,4)
+    ry = random.randint(-4,4)
+    if rx < 0 and ry < 0:
+        new_img = img[:, :ry, :rx, :]
+        new_img = np.pad(new_img, ((0, 0), (-ry, 0), (-rx, 0), (0, 0)), 'constant')
+    elif rx >= 0 and ry >= 0:
+        new_img = img[:, ry:, rx:, :]
+        new_img = np.pad(new_img, ((0, 0), (0, ry), (0, rx), (0, 0)), 'constant')
+    elif rx < 0 and ry >= 0:
+        new_img = img[:, ry:, :rx, :]
+        new_img = np.pad(new_img, ((0, 0), (0, ry), (-rx, 0), (0, 0)), 'constant')
+    else:
+        new_img = img[:, :ry, rx:, :]
+        new_img = np.pad(new_img, ((0, 0), (-ry, 0), (0, rx), (0, 0)), 'constant')
+    return new_img
+
+
+def build_conf_matrix(y_, labels, conf_matrix):
+    y_index = np.argmax(y_, axis=1)
+    l_index = np.argmax(labels, axis=1)
+    np.add.at(conf_matrix, [l_index, y_index], 1)
+    return conf_matrix
+
+
+def calc_metrics(conf_matrix):
+    pass
+
+data_dir = os.path.join('data', 'emnist-letters')
+
+dataset = extract_data(data_dir)
+
+settings = {'conv1': [5, 5, 1, 8],     # output 14x14x16
+            'conv2': [5, 5, 8, 16],    # output 7x7x32
+            'conv3': [3, 3, 16, 32],    # output 7x7x128
+            'flat':  [7, 7, 32],
+            'full1': [512],
+            'full2': [CLASSES]}
+
+batch_size = 32
+epochs = 30
 learning_rate = 0.001
-batch_size = 64
-epochs = 300
+steps = [5, 10, 15, 30]
+scale = [0.1, 0.5, 0.1, 0.5]
+momentum = 0.9
 
 
-input_placeholder = tf.placeholder(tf.float32)
-preds = model.create_classifier(input_placeholder, settings, training=True)
+input_placeholder = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+train_placeholder = tf.placeholder(tf.bool)
+keep_prob_placeholder = tf.placeholder(tf.float32)
+preds = model.create_classifier(input_placeholder, settings,
+                                keep_prob=keep_prob_placeholder,
+                                training=train_placeholder)
 
 label_placeholder = tf.placeholder(tf.float32)
+cross_entrophy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=label_placeholder,
+                                                                           logits=preds))
 
-cross_entrophy = tf.reduce_mean(
-                 tf.nn.softmax_cross_entropy_with_logits_v2(labels=label_placeholder, logits=preds))
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entrophy)
+lr_placeholder = tf.placeholder(tf.float32)
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(extra_update_ops):
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entrophy)
 
-correct = tf.equal(tf.argmax(label_placeholder, axis=1), tf.argmax(preds, axis=1))
+softmax = tf.nn.softmax(preds)
+correct = tf.equal(tf.argmax(label_placeholder, axis=1), tf.argmax(softmax, axis=1))
 accuracy = tf.reduce_mean(tf.cast(correct, dtype=tf.float32))
+num_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
 
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    num_batches = int(len(dataset['training']) / batch_size)
+    train_imgs = dataset['train'][0]
+    train_labels = dataset['train'][1]
+
+    val_imgs = dataset['val'][0]
+    val_labels = dataset['val'][1]
+
+    test_imgs = dataset['test'][0]
+    test_labels = dataset['test'][1]
+
+    num_train_batches = int(train_imgs.shape[0] / batch_size)
+    val_batch_size = int(val_imgs.shape[0] / 5)
+    num_val_batches = int(val_imgs.shape[0] / val_batch_size)
+    val_imgs, val_labels = shuffle_data(val_imgs, val_labels)
+    c_val_batch = 0
+    i = 0
     for e in range(epochs):
-        print('\repoch: %d' % e, end='')
-        if e % int(epochs/10) == 0:
-            v_batch, v_labels = load_data(dataset['val'], labels)
-            v_accuracy = accuracy.eval(feed_dict={input_placeholder: v_batch,
-                                                  label_placeholder: v_labels})
-            print('\nepoch: %d validation accuracy: %g' % (e, v_accuracy))
-        dataset['training'].sort()
-        for b in range(num_batches):
+        print('\nepoch: %d/%d' % (e, epochs))
+        if e % 1 == 0:
+            if c_val_batch == 5:
+                c_val_batch = 0
+                val_imgs, val_labels = shuffle_data(val_imgs, val_labels)
+            start = c_val_batch * val_batch_size
+            end = start + val_batch_size
+            v_batch = val_imgs[start:end]
+            v_batch = shift_img(v_batch)
+            v_labels = val_labels[start:end]
+            v_accuracy, loss = sess.run([accuracy, cross_entrophy], feed_dict={input_placeholder: v_batch,
+                                                                               train_placeholder: False,
+                                                                               keep_prob_placeholder: 1.0,
+                                                                               label_placeholder: v_labels})
+            print('\nepoch: %d validation accuracy: %g\tloss: %g' % (e, v_accuracy, loss))
+            c_val_batch += 1
+
+        train_imgs, train_labels = shuffle_data(train_imgs, train_labels)
+
+        if e in steps:
+            learning_rate *= scale[i]
+            i += 1
+        for b in range(num_train_batches):
             start = b * batch_size
             end = start + batch_size
-            t_batch, t_labels = load_data(dataset['training'][start:end], labels)
+            t_batch = train_imgs[start:end]
+            t_batch = shift_img(t_batch)
+            t_labels = train_labels[start:end]
             train_step.run(feed_dict={input_placeholder: t_batch,
-                                      label_placeholder: t_labels})
+                                      train_placeholder: True,
+                                      keep_prob_placeholder: 0.5,
+                                      label_placeholder: t_labels,
+                                      lr_placeholder: learning_rate})
+            print('\rtraining batch: %d/%d' % (b, num_train_batches), end='')
 
-    test_input, test_labels = load_data(dataset['test'], labels)
-    test_accuracy = accuracy.eval(feed_dict={input_placeholder: test_input,
-                                             label_placeholder: test_labels})
+    num_test_iters = int(test_imgs.shape[0] / 10)
+    test_accuracy = 0
+    conf_matrix = np.zeros((CLASSES, CLASSES), dtype=np.int32)
+    for t in range(num_test_iters):
+        start = t * num_test_iters
+        end = start + num_test_iters
+        t_input = test_imgs[start:end]
+        t_labels = test_labels[start:end]
+        t_correct, y_ = sess.run([num_correct, preds],feed_dict={input_placeholder: t_input,
+                                                                   train_placeholder: False,
+                                                                   keep_prob_placeholder: 1.0,
+                                                                   label_placeholder: t_labels})
+
+        test_accuracy += t_correct
+        conf_matrix = build_conf_matrix(y_, t_labels, conf_matrix)
+
+    test_accuracy /= float(num_test_iters * 10)
     print('\ntest accuracy: %g' % test_accuracy)
     print('saving model..')
     save_dir = os.path.join('saved_model', 'c_%g' % test_accuracy)
     saver.save(sess, save_dir)
+    #print(conf_matrix)
